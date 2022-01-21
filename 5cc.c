c9c95a33c416503b63da10090c9b6a3f5406d7d6
skydder <skydder.shoma@gmail.com>
2022-01-21 20:15:13 +0900

    加減算のできるコンパイラの作成

--- 5cc.c
+++ 5cc.c
@@ -7,11 +7,30 @@ int main(int argc, char **argv) {
         return 1;
     }
     
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("\tmov rax, %d\n", atoi(argv[1]));
-    printf("\tret\n");
+    printf("\tmov rax, %ld\n", strtol(p, &p, 10));
     
+    while (*p) {
+        if (*p == '+') {
+	    p++;
+	    printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+	    continue;
+	}
+
+	if (*p == '-') {
+	    p++;
+	    printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+	    continue;
+	}
+	
+	fprintf(stderr, "予期しない文字です。: '%c'\n", *p);
+	return 1;
+    }
+
+    printf("\tret\n");
     return 0;
 }
