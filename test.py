971af670b9e6f97c0749c6b39a3e9fc8e1e588a3
skydder <skydder.shoma@gmail.com>
2022-04-17 20:15:02 +0900

    write test in python

---
+++ test.py
@@ -0,0 +1,20 @@
+import subprocess, sys
+
+def test_compiler(string, expect):
+    asm = subprocess.run(['./5cc', string], capture_output=True).stdout.decode('utf-8')
+    with open('tmp.s', 'w', encoding='utf-8') as file:
+        file.write(asm)
+    subprocess.run(['cc', '-o', 'tmp', 'tmp.s'])
+    result = subprocess.run(['./tmp']).returncode
+    if result != expect:
+        err = string + " => " + str(expect) + "expected, but got " + str(result)
+        sys.stdout.write(err)
+    else:
+        out = string + " => " + str(result) + "\n"
+        sys.stdout.write(out)
+
+def main():
+    test_compiler("int main() {return 0;}", 0)
+
+if __name__ == '__main__':
+    main()
