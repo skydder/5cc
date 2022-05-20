759a6dfd07b01e64283d1f4df4dddbe92e41a1c9
skydder <skydder.shoma@gmail.com>
2022-05-20 16:54:46 +0900

    little change test.py

--- test/test.py
+++ test/test.py
@@ -5,17 +5,22 @@ def test_compiler(string, expect):
     with open('tmp.s', 'w', encoding='utf-8') as file:
         file.write(asm)
     subprocess.run(['cc', '-o', 'tmp', 'tmp.s'])
-    result = subprocess.run(['./tmp']).returncode
-    if result != expect:
-        err = string + " => " + str(expect) + " expected, but got " + str(result) + "\n"
-        sys.stdout.write(err)
-    else:
-        out = string + " => " + str(result) + "\n"
-        sys.stdout.write(out)
+    try:
+        result = subprocess.run(['./tmp']).returncode
+        if result != expect:
+            out = f"{string} => {expect} expected, but got {result}\n"
+            sys.stdout.write(out)
+        else:
+            out = f"{string} => {result} \n"
+            sys.stdout.write(out)
+    except FileNotFoundError:
+        sys.stdout.write("FileNotFoundError: [Errno 2] No such file or directory: './tmp'\n")
 
 def main():
     print("===================================================================")
     test_compiler("int main() {return 0;}", 0)
+    test_compiler("int main() {return 0 + 1;}", 1)
+    test_compiler("int main() {int a;int *b; int c;a =1; b = &a; c =*(&b+1);return c;}", 1)
     test_compiler("int main(){int a; a = 0; return a;}", 0)
     test_compiler("int fib(int n) {return n;} int main() {return fib(2);}", 2)
     test_compiler("int main() {int *a;int x; x = 10; a= &x;return *a;}", 10)
